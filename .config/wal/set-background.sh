b7ff39f5d93a7cfda9129edfff5478f526afadc1
eggstrom <tristan65536@gmail.com>
2023-10-08 02:26:05 +0200

    Put Rofi colors and font into separate files

--- .config/wal/set-background.sh
+++ .config/wal/set-background.sh
@@ -15,5 +15,5 @@ feh --no-fehbg --bg-fill ~/.cache/background.png
 mv ~/.cache/wal/colors-alacritty.yml ~/.config/alacritty/colors.yml
 printf "font:\n  size: $(xrdb -query | grep -F 'alacritty.fontSize' | cut -f2)" > ~/.config/alacritty/font-size.yml
 
-mv ~/.cache/wal/colors-rofi-dark.rasi ~/.config/rofi/theme.rasi
-sed -i "s/\$fontsize/$(xrdb -query | grep -F 'rofi.fontSize' | cut -f2)/" ~/.config/rofi/theme.rasi
+mv ~/.cache/wal/colors-rofi-dark.rasi ~/.config/rofi/colors.rasi
+printf "* {\n	font: \"JetBrains Mono Nerd Font $(xrdb -query | grep -F 'rofi.fontSize' | cut -f2)\";\n}" > ~/.config/rofi/font.rasi

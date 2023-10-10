793218f1e48f5d08f3f2f804e5a031db551b9629
eggstrom <tristan65536@gmail.com>
2023-10-11 01:15:59 +0200

    Change theme menu

--- .config/rofi/scripts/theme.sh
+++ .config/rofi/scripts/theme.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
-option=$(~/.config/rofi/scripts/menu.sh "$(find ~/.config/themes -maxdepth 1 -type d | tail -n+2 | cut -d/ -f6 | sort)")
+declare -A themes=(
+	['Catppuccin Frappe']='catppuccin-frappe'
+	['Catppuccin Latte']='catppuccin-latte'
+	['Catppuccin Macchiato']='catppuccin-macchiato'
+	['Catppuccin Mocha']='catppuccin-mocha'
+	['Dracula']='dracula'
+	['Wal']='wal'
+)
 
-[[ -n "$option" ]] && ~/.config/themes/set-theme.sh "$option"
+for theme in "${!themes[@]}"; do
+	options+="$theme\n"
+done
+options=$(printf "$options" | sort)
+
+option=$(~/.config/rofi/scripts/menu.sh "${options}")
+[[ -n "$option" ]] && ~/.config/themes/set-theme.sh "${themes[$option]}"

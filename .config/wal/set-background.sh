362cdb387143aeb5ee0b8318cadfbc687a39a68f
eggstrom <tristan65536@gmail.com>
2023-10-10 14:48:25 +0200

    Use rational file naming conventions

--- .config/wal/set-background.sh
+++ .config/wal/set-background.sh
@@ -3,7 +3,7 @@
 wal -qntsei "$1"
 feh --no-fehbg --bg-fill --force-aliasing "$1"
 
-mkdir ~/.config/themes/Wal
-mv ~/.cache/wal/colors.Xresources ~/.cache/wal/alacritty.yml ~/.cache/wal/rofi.rasi ~/.config/themes/Wal
+mkdir ~/.config/themes/wal
+mv ~/.cache/wal/colors.Xresources ~/.cache/wal/alacritty.yml ~/.cache/wal/rofi.rasi ~/.config/themes/wal
 
-[[ $(readlink ~/.config/themes/current) = ~/.config/themes/Wal ]] && ~/.config/themes/reload-colors.sh
+[[ $(readlink ~/.config/themes/current) = ~/.config/themes/wal ]] && ~/.config/themes/reload-colors.sh

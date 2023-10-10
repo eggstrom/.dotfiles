09daaf09a94a803c5312c0e02fdf54abb054d6db
eggstrom <tristan65536@gmail.com>
2023-10-10 03:32:44 +0200

    Change some Polybar things

--- .config/wal/set-background.sh
+++ .config/wal/set-background.sh
@@ -4,8 +4,5 @@ feh --no-fehbg --bg-fill --force-aliasing "$1"
 mv ~/.cache/wal/colors-alacritty.yml ~/.config/alacritty/colors.yml
 printf "font:\n  size: $(xrdb -query | grep -F 'alacritty.fontSize' | cut -f2)" > ~/.config/alacritty/font-size.yml
 
-mv ~/.cache/wal/colors-polybar.ini ~/.config/polybar/colors.ini
-touch ~/.config/polybar/config.ini
-
 mv ~/.cache/wal/colors-rofi.rasi ~/.config/rofi/colors.rasi
 printf "* {\n	font: \"JetBrains Mono Nerd Font $(xrdb -query | grep -F 'rofi.fontSize' | cut -f2)\";\n	border: $(xrdb -query | grep -F 'border' | cut -f2);\n}" > ~/.config/rofi/font.rasi

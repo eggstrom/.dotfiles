41980046e5e5daf69b75c9f911e8187f97673e22
eggstrom <tristan65536@gmail.com>
2023-10-10 08:32:43 +0200

    Make it so you can switch themes

--- .config/wal/set-background.sh
+++ .config/wal/set-background.sh
@@ -1,8 +1,9 @@
 wal -nqti "$1"
 feh --no-fehbg --bg-fill --force-aliasing "$1"
 
-mv ~/.cache/wal/colors-alacritty.yml ~/.config/alacritty/colors.yml
+mv ~/.cache/wal/colors-alacritty.yml ~/.cache/wal/colors-rofi.rasi ~/.config/themes/Wal
+cp ~/.cache/wal/colors.Xresources ~/.config/themes/Wal
+
 printf "font:\n  size: $(xrdb -query | grep -F 'alacritty.fontSize' | cut -f2)" > ~/.config/alacritty/font-size.yml
 
-mv ~/.cache/wal/colors-rofi.rasi ~/.config/rofi/colors.rasi
 printf "* {\n	font: \"JetBrains Mono Nerd Font $(xrdb -query | grep -F 'rofi.fontSize' | cut -f2)\";\n	border: $(xrdb -query | grep -F 'border' | cut -f2);\n}" > ~/.config/rofi/font.rasi

27d671f2ab647711b540b11b7c27ba2bd8d43aff
eggstrom <tristan65536@gmail.com>
2023-10-11 06:35:34 +0200

    Simplify files required for theme definition

--- .config/themes/reload-colors.sh
+++ .config/themes/reload-colors.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
-xrdb -merge ~/.config/themes/current/colors.Xresources
+# Get colors
+background=$(cat ~/.config/themes/current/colors | grep '^background: ' | cut -d' ' -f2)
+foreground=$(cat ~/.config/themes/current/colors | grep '^foreground: ' | cut -d' ' -f2)
 
+# Load colors into Xresources
+echo "background: #$background"          | xrdb -merge
+echo "foreground: #$foreground"          | xrdb -merge
+echo "polybar.background: E6$background" | xrdb -merge
+
+# Create Rofi file for colors
+printf "* {\n\tbackground: #${background}E6;\n\tforeground: #$foreground;\n}" > ~/.config/rofi/colors.rasi
+
+# Reload programs
 i3-msg reload
 touch ~/.config/polybar/config.ini
 touch ~/.config/alacritty/alacritty.yml
--- .config/wal/set-background.sh
+++ .config/wal/set-background.sh
@@ -4,6 +4,6 @@ wal -qntsei "$1"
 feh --no-fehbg --bg-fill --force-aliasing "$1"
 
 mkdir ~/.config/themes/wal
-mv ~/.cache/wal/colors.Xresources ~/.cache/wal/alacritty.yml ~/.cache/wal/rofi.rasi ~/.config/themes/wal
+mv ~/.cache/wal/colors ~/.cache/wal/alacritty.yml ~/.config/themes/wal
 
 [[ $(readlink ~/.config/themes/current) = ~/.config/themes/wal ]] && ~/.config/themes/reload-colors.sh

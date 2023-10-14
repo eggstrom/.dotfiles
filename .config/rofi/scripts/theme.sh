ff0cbb690a8c8d7fefc25c14392b76050ff69431
eggstrom <tristan65536@gmail.com>
2023-10-14 03:35:24 +0200

    Make theme menu open on currently chosen theme

--- .config/rofi/scripts/theme.sh
+++ .config/rofi/scripts/theme.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Define names and files for themes
 declare -A themes=(
 	['Catppuccin Frappé']='catppuccin-frappe'
 	['Catppuccin Latte']='catppuccin-latte'
@@ -15,10 +16,22 @@ declare -A themes=(
 	['Wal (Light)']='wal-light'
 )
 
+# Create sorted list of themes
 for theme in "${!themes[@]}"; do
 	options+="$theme\n"
 done
 options=$(printf "$options" | sort)
 
-option=$(~/.config/rofi/scripts/menu.sh "$options")
+# Get index of current theme
+readarray -t optionsArray <<< "$options"
+current="$(basename "$(readlink ~/.config/theme/current)")"
+i=0
+for theme in "${optionsArray[@]}"; do
+	[[ "${themes[$theme]}" == "$current" ]] && break;
+	((i=i+1))
+done
+echo "$i"
+
+# Prompt user to pick theme and set it if one was chosen
+option=$(printf "$options" | rofi -dmenu -no-custom -i -p '' -selected-row "$i")
 [[ -n "$option" ]] && ~/.config/theme/set-theme.sh "${themes[$option]}"

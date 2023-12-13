44966f3a91a96bddb6e5caa7385cac7ed5188a3e
eggstrom <tristan65536@gmail.com>
2023-12-13 15:02:03 +0100

    Add borders to Lazy

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,3 +1,4 @@
+-- Bootstrap Lazy
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system {
@@ -10,4 +11,6 @@ if not vim.loop.fs_stat(lazypath) then
     }
 end
 vim.opt.rtp:prepend(lazypath)
-require('lazy').setup('plugins/')
+
+-- Configure Lazy
+require('lazy').setup('plugins/', { ui = { border = 'single' } })

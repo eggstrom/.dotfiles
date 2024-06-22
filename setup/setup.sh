8ffc0be6c5fc8e8a05e6620f682b7b8fa4cd3ab6
eggstrom <tristan65536@gmail.com>
2024-06-22 09:48:59 +0200

    Add a bit to the setup script

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,4 +2,27 @@
 
 cd "$(dirname "$0")" || exit 1
 
-sudo pacman -Syu $(sed 's/#.*//g' <pacman.txt)
+# Install packages from official repositories
+xargs sudo pacman -Syu <pacman.txt
+
+# Install AUR helper
+git clone https://aur.archlinux.org/yay-bin.git
+(
+    cd yay-bin || exit 1
+    makepkg -sicr yay-bin
+)
+rm -rf yay-bin
+
+# Install AUR packages
+xargs yay -S <aur.txt
+
+# Change shell for user and root to Zsh
+sudo chsh -s /usr/bin/zsh
+sudo chsh -s /usr/bin/zsh root
+
+# Stow dotfiles without making directory symlinks
+(
+    cd ..
+    fd -utd --base-directory home | xargs -I {} mkdir -vp ~/{}
+    stow -t ~ home
+)
